5fa1ed9117ec707326ff1e23d5f366e7e830d649
Tyler <tyler.vilme@uconn.edu>
2026-09-11 22:11:19 -0400

    feat: added work for hw2

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
+        learning_rate=0.0000001,
+        n_estimators=500,
+        max_depth=None,
+        subsample=0.00001,
+        min_samples_leaf=3,
         random_state=seed
     )
     model.fit(X, y)
