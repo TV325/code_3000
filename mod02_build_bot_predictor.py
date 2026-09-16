e8bfc0d069f3d61f3ab579d4c8e8cecd26afe64f
Tyler <tyler.vilme@uconn.edu>
2026-09-16 13:27:01 -0400

    changed gradient boosting attributes

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.0000001,
-        n_estimators=500,
+        learning_rate=0.000001,
+        n_estimators=100,
         max_depth=None,
-        subsample=0.00001,
-        min_samples_leaf=3,
+        subsample=0.00000001,
+        min_samples_leaf=100000000000000,
         random_state=seed
     )
     model.fit(X, y)
